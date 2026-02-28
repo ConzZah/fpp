520842aa19b4d8e375d2c3684090d475d51cfb8b
ConzZah <69615452+ConzZah@users.noreply.github.com>
2026-02-28 02:21:11 +0100

    v1.5
    === v1.5 === 
    
    - better arg processing
    - better url detection
    - other small fixes

--- firefox.sh
+++ firefox.sh
@@ -3,22 +3,22 @@
 #===============================================
 # Project: fpp
 # Author:  ConzZah
-# Last Modification: 2/3/26 5:20 AM
+# Last Modification: 2/27/26 10:30 PM
 #===============================================
 
 # shellcheck disable=SC2009 # REASON: pgrep is not POSIX
-# shellcheck disable=SC2012 # REASON: THERE ARE NO NON-ALPHANUMERIC FILENAMES WE'D NEED TO WORRY ABOUT
+# shellcheck disable=SC2012 # REASON: THERE ARE NO NON-ALPHANUMERIC FILENAMES WE'D NEED TO WORRY ABOUT 
 
-[ "$1" = "-h" ] && echo '
+help () { echo '
 ===============================
- FPP v1.4 /// ConzZah /// 2026 
+ FPP v1.5 /// ConzZah /// 2026
 ===============================
 
 OPTIONS:
 
--o      USE YOUR OWN FIREFOX PROFILE
+-m      USE MOST RECENT FIREFOX PROFILE
 
--p      SPECIFY CUSTOM PATH TO FIREFOX PROFILE
+-p      SPECIFY PATH TO FIREFOX PROFILE
 
 -fr     CREATE FIRSTRUN FLAG
 
@@ -28,41 +28,55 @@ OPTIONS:
 
 [URL]   OPEN ANY URL
 
-NOTES:
+'; exit ;}
 
-ANY OPTIONS MUST BE SPECIFIED BEFORE THE URL
-' && exit 0
-
+foxes="firefox-esr firefox librewolf"
+firefox=""; path2profile="$(pwd)/fpp"
+url="about:newtab" ### <-- default url
 pathnotfound="--> ERROR: COULDN'T FIND PATH TO PROFILE"
 
-path2profile="$(pwd)/fpp"; firefox=""
 ### check if firefox is even installed & exit if it shouldn't be
-command -v firefox-esr >/dev/null && firefox="firefox-esr"
-[ -z "$firefox" ] && command -v firefox >/dev/null && firefox="firefox"
-[ -z "$firefox" ] && command -v librewolf >/dev/null && firefox="librewolf"
+for fox in $foxes; do
+command -v "$fox" >/dev/null && firefox="$fox"; done
 [ -z "$firefox" ] && echo "--> ERROR: FIREFOX APPEARS TO BE MISSING." && exit 1
 
-### check if the user wants to use their own profile and
+#### PROCESS ARGS ####
+while [ "$#" -gt "0" ]; do
+case $1 in
+
+m|M'-m'|'-M')
 ### find the path to the most recently used firefox profile
-[ "$1" = "-o" ] && shift && {
 [ ! -d "$HOME/.mozilla/firefox/" ] && echo "$pathnotfound" && exit 1
-path2profile="$(ls -t "$HOME"/.mozilla/firefox/*/compatibility.ini| head -n1| sed 's#compatibility.ini##g')"
-}
+path2profile="$(ls -t "$HOME"/.mozilla/firefox/*/compatibility.ini| head -n1| sed 's#compatibility.ini##g')"; shift 
+;;
 
-### check if the user wants to specify a custom path to a profile
-[ "$1" = '-p' ] && shift && {
-path2profile="$1"
-[ ! -d "$path2profile" ] && \
-echo "$pathnotfound" && exit 1
-}
+p|P|'-p'|'-P') 
+### if the user wants to specify a path to a profile, check if it exists
+shift; [ -d "$1" ] && path2profile="$1" && shift || \
+[ ! -d "$1" ] && echo "$pathnotfound" && exit 1
+;;
 
+k|K|'-kill')
 ### kill firefox if the user asks for it
-[ "$1" = "-kill" ] && {
 [ ! -f ".pid" ] && echo '--> ERROR: NO .PID FILE FOUND' && exit 1
 ps -aux| grep -v grep| grep -qo "$(cat .pid)" && \
 echo '--> KILLING FIREFOX PROCESS..' && kill -15 "$(cat .pid)"
 [ -f ".pid" ] && rm -f .pid; exit
-}
+;;
+
+fr|'-fr')
+### if $1 is '-fr', create firstrun flag
+touch "$path2profile/.fr" && shift
+;;
+
+### check if "$1" is pingable and overwrite "$url" if so
+*.*) ping -c 1 -W 0.5 "$1" >/dev/null && url="$1"; shift ;; 
+
+### help, when needed
+h|'-h'|'help') help ;;
+*) help ;;
+esac
+done
 
 ### if $path2profile couldn't be found, and is equal to the default,
 ### check if the 7z archive is present, if not, download & extract it, and create .fr
@@ -78,9 +92,6 @@ curl -#LO 'https://github.com/ConzZah/fpp/raw/refs/heads/main/fpp.7z'
 ### delete addonStartup.json.lz4, to make addon/extension paths fix themselves
 rm -f "$path2profile/addonStartup.json.lz4" >/dev/null
 
-### if $1 is '-fr', create firstrun flag
-[ "$1" = '-fr' ] && touch "$path2profile/.fr" && shift
-
 ### if .fr (firstrun flag) does exist, perform first run
 [ -f ".fr"  ] || [ -f "$path2profile/.fr" ] && {
 echo '--> PERFORMING FIRST RUN, PLS WAIT..'
@@ -88,11 +99,6 @@ rm -f "$path2profile/.fr" ".fr" >/dev/null
 rm -f "$path2profile/sessionstore.jsonlz4" >/dev/null
 rm -rf "$path2profile/sessionstore-backups" >/dev/null
 }
-
-url="about:newtab" ### <-- default url
-### if "$1" seems to be an url,
-### treat it as one & overwrite default
-echo "$1"| grep -q '.*://.*' && url="$1"
 
 #### LAUNCH ####
 launch="$firefox $url --allow-downgrade --profile $path2profile"
